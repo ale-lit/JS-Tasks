45127dd13cf79d82e873e53bfe93158716b85d26
ZeldoR <zeldor@ya.ru>
2021-11-04 16:20:46 +0300

    refact: change code style

---
+++ 01-palindrome/script.js
@@ -0,0 +1,47 @@
+/*
+ * Задача 1: «Палиндром»
+ *
+ * Палиндром — это слово, предложение или последовательность символов,
+ * которая читается слева направо так же, как и справа налево. Например,
+ * «топот» и «Анна» — палиндромы, а «привет» и «Витя» — нет.
+ *
+ * Напишите функцию palindrome(str), принимающую как аргумент строку.
+ * Функция должна вернуть true, если строка — палиндром, и false, если нет.
+ * 
+ * Считайте, что на вход всегда передаётся слово: то есть знаков препинания
+ * и пробелов в аргументе быть не может.
+ * 
+*/
+
+function palindrome(str) { 
+    // To Lower Case
+    let newStr = str.toLowerCase();
+    // Delete Excess Sym
+    newStr = newStr.replace(/[.,\/#\s!$%\^&\*;:{}=\-_`~()]/g,'');
+    // Convert To Array
+    let strArr = newStr.split('');
+    // Reverse Array
+    strArr.reverse();
+    // Convert To Str
+    reverseStr = strArr.join('');
+
+    //
+    console.log(str + ': ' + (newStr === reverseStr));
+    return newStr === reverseStr;
+}
+
+// Протестируйте решение, вызывая функцию с разными аргументами:
+
+console.log(palindrome('топот')); // должно быть true
+console.log(palindrome('Saippuakivikauppias')); // true
+console.log(palindrome('привет')); // false
+
+/*
+ * Бонус. Задача для любознательных. Пусть функция принимает на вход любую строку,
+ * но пробелы и знаки препинания не учитывает. Например:
+ * 
+ * palindrome('О, лета тело!'); // true
+ * 
+*/
+
+console.log(palindrome('О, лета тело!')); // true
